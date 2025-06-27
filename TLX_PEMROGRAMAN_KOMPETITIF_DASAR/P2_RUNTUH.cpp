5798fa055e8e4abca23639fde832c6723ec0ee04
SirPseudocode <dafrad.tya@gmail.com>
2025-06-27 22:21:19 +0800

    H+3 Friday, 27th 2025

--- TLX_PEMROGRAMAN_KOMPETITIF_DASAR/P2_RUNTUH.cpp
+++ TLX_PEMROGRAMAN_KOMPETITIF_DASAR/P2_RUNTUH.cpp
@@ -1,53 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+string papan[20];
+string satu, nol;
+int r, c;
 
-    short r,c;
-    cin >> r >> c;
-
-    string arr[r];
-    for(short i = 0; i < r; i++){
-        cin >> arr[i];
+bool masihruntuh() {
+    for(int i=0; i<r; i++) {
+        if(satu == papan[i]) return true;
     }
+    return false;
+}
 
-    bool terhapus;
-    do{
-        terhapus = false;
-        for(short i = 0; i < r; i++){
-            bool penuh = true;
-            for(short j = 0; j < c; j++){
-                if(arr[i][j] == '0'){
-                    penuh = false;
-                    break;
-                }
-            }
-            if(penuh){
-                terhapus = true;
-                for(short j = 0; j < c; j++){
-                    arr[i][j] = '0';
-                }
-                for(short k = 0; k < c; k++){
-                    short dasar = i;
-                    for(short l = i; l >= 0; l--){
-                        if(arr[l][k] == '1'){
-                            arr[dasar][k] = '1';
-                            if(dasar != l){
-                                arr[l][k] = '0';
-                            }
-                            dasar--;
-                        }
-                    }
-                }
+void runtuhkan() {
+    short lastsatu = 0;
+    for(int i=0; i<r; i++) {
+        if(papan[i] == satu) {
+            papan[i] = nol;
+            lastsatu = i;
+        }
+    }
+    for(int i=0; i<c; i++) {
+        short berapasatu = 0, lowestsatu = r;
+        for(int j=0; j<lastsatu; j++) {
+            if(papan[j][i] == '1') {
+                berapasatu++;
+                papan[j][i] = '0';
             }
         }
-    }while(terhapus);
-    
-    for(short i = 0; i < r; i++){
-        cout << arr[i] << '\n';
+        for(int j=(lastsatu+1); j < r; j++) {
+            if(papan[j][i] == '1') {
+                lowestsatu = j;
+                break;
+            }
+        }
+        for(int j=(lowestsatu-1); ((j>=0) && (berapasatu > 0)); j--) {
+            berapasatu--;
+            papan[j][i] = '1';
+        }
     }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>r>>c;
+    for(int i=0; i<r; i++)
+        cin>>papan[i];
+    for(int i=0; i<c; i++) {
+        satu += "1";
+        nol += "0";
+    }
+    while(masihruntuh())
+        runtuhkan();
+    for(int i=0; i<r; i++)
+        cout<<papan[i]<<endl;
     return 0;
 }
