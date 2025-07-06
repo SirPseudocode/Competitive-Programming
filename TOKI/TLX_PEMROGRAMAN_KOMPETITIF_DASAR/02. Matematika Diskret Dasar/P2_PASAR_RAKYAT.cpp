2bf877cdf851590a94c7714962e1b8a76f22d4c0
SirPseudocode <dafrad.tya@gmail.com>
2025-07-06 23:29:01 +0800

    H+12 Sunday, 06th July 2025

---
+++ TOKI/TLX_PEMROGRAMAN_KOMPETITIF_DASAR/02. Matematika Diskret Dasar/P2_PASAR_RAKYAT.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <numeric>
+#define ll long long
+#define ull unsigned long long
+#define unset unordered_set
+#define mset multiset
+#define v vector
+#define f first
+#define s second
+#define pb push_back
+using namespace std;
+const int mod = 1e9 + 7;
+
+int gcd(int x, int y){
+    if(y == 0) return x;
+    return gcd(y , x % y);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+    n--;
+    while(n--){
+        int in, f;
+        cin >> in;
+        f = gcd(x,in);
+        x *= in;
+        x /= f;
+    }
+    cout << x << '\n';
+    return 0;
+}
