25d85cc3458cb122ae488cdb1b81200e9601083c
SirPseudocode <dafrad.tya@gmail.com>
2025-07-27 09:58:52 +0800

    H+33 Sunday, 27th July 2025

---
+++ VJudge/OSDIUM/Latihan Soal Greedy/MEMBANGUN_MENARA.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <numeric>
+#define ll long long
+#define ull unsigned long long
+#define unset unordered_set
+#define mset multiset
+#define v vector
+#define fi first
+#define se second
+#define pb push_back
+using namespace std;
+const int mod = 1e9 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+}
